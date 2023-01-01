f6ba2d3b5a8e7e308c5c68954359475a6198fc1a
Sven Graupner <sgraupner@bht-berlin.de>
2023-01-01 18:03:12 +0000

    added context help to .vscode/vscode_memo.sh

--- .vscode/vscode_memo.sh
+++ .vscode/vscode_memo.sh
@@ -6,6 +6,7 @@
 # open second GUI:          Command Palette + Duplicate As Workspace in new Window
 # open Markup preview:      Ctrl + Shift + V
 # run code:                 Ctrl + Alt + N
+# context help:             Ctrl + .
 #
 
 # "code-runner.executorMap": {
